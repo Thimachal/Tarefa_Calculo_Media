63338a298dd4c89f9309cb0cc055a7a1b473bdbf
Thimachal <thimachal@gmail.com>
2023-06-28 20:51:24 -0300

    atividade ebac

---
+++ src/CalcAverage.java
@@ -0,0 +1,20 @@
+public class CalcAverage{
+    double average1;
+    double average2;
+    double average3;
+    double average4;
+
+    public CalcAverage(double average1, double average2, double average3, double average4) {
+        this.average1 = average1;
+        this.average2 = average2;
+        this.average3 = average3;
+        this.average4 = average4;
+    }
+
+    public void printCalc(){
+        double average = (this.average1+this.average2+this.average3+this.average4)/4;
+        System.out.println("A média das 4 notas: " + average);
+        if(average >= 7) System.out.println("Aprovado!!!");
+        else System.out.println("Reprovado!!!");
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        CalcAverage student1 = new CalcAverage(8.5, 7.0, 9.0, 6.5);
+        //CalcAverage student2 = new CalcAverage(4.5, 4.0, 9.0, 6.5);
+        student1.printCalc();
+        //student2.printCalc();
+    }
+}
